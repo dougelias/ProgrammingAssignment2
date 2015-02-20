db62605522bf24ee210a8a688ad48b45d17dc62f
doug.elias <doug.elias@gmail.com>
2015-02-20 11:48:42 -0500

    Submitting modified cachematrix.R
    N.B.: code has NOT been checked for executable-correctness

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Create a special 'matrix' object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+    ## initialize object
+    m <- NULL
 
+    ## modify objects in external environment
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+
+    ## functions on the object (self-explanatory)
+    get <- function() x
+    setinv <- function(cacheSolve) m <<- cacheSolve
+    getinv <- function() m
+
+    ## what makeCacheMatrix returns
+    ## (an anonymous list providing
+    ## access to all of the functions
+    ## on the object
+    list(set = set,
+         get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
+## Calculates the inverse of the
+## 'special' matrix created above
 
-## Write a short comment describing this function
+cacheinv <- function(x, ...) {
+    
+        ## Check to see if the inverse has already been
+        ## calculated and ... 
+        if(!is.null(m)) {
+            
+            ## ... if so, gets the inverse from the cache ...
+            m <- x$getinv()
+            message("getting cached data")
+
+            ## and skip the computation
+            return(m)
+        }
+        
+        ## ... else calculate the inverse ...
+        data <- x$get()
+        m <- cacheSolve(data, ...)
+
+        ## ...modify the object in the
+        ## external environment, and ...        
+        x$setinv(m)
+
+        ## ...return the object
+        m
+}
+
+## Return the inverse of x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    return (solve(x))
 }
